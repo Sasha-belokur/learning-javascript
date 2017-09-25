93d091e6300e321b825a6d597927da69d25b62f6
Alex Belokur <alex.belokur17@gmail.com>
2017-09-25 21:58:10 +0300

    Function findUniqueStrings

---
+++ findUniqueStrings.js
@@ -0,0 +1,21 @@
+/*Write a function unique (arr), that returns an array containing only the unique elements of arr.*/
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", "8-()"
+];
+
+findUniqueStrings(strings)
+
+function findUniqueStrings(arr) {
+  let obj = {}
+  for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = true;
+  }
+  let uniqueStrings = [];
+
+  for (str in obj) {
+    uniqueStrings.push(str);
+  } 
+
+return uniqueStrings;
+}
